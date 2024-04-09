08ec447eb2fcbf9953c3d65745c80ca1f64ac213
LeonardoSoares09 <leoss6770@gmail.com>
2024-04-09 10:51:35 -0300

    commit 2

--- main.py
+++ main.py
@@ -1,24 +1,75 @@
 mapa = []
 
+
 def contar_dinheiro(mapa):
     count = 0
     current = 'D'
-    x = 0 
+    x = 0
     c = 0
     y = 0
-
+    numero_completo = 0
     for i in mapa:
         if i[0][0] == '-':
             x += c    
             break
         c += 1
 
+
     while mapa[x][y] != '#':
+       
+        if  mapa[x][y].isdigit() and mapa[x][y - 1].isdigit() and mapa[x][y - 2].isdigit() and mapa[x][y - 3].isdigit() and current == 'E':
+            numero_completo = mapa[x][y - 3] + mapa[x][y - 2] + mapa[x][y - 1] + mapa[x][y] 
+            print(int(numero_completo), end=(', '))
+            count += int(numero_completo)
+            mapa[x][y] = 'x'
+            mapa[x][y - 1] = 'x'
+            mapa[x][y - 2] = 'x'
+            mapa[x][y - 3] = 'x'
+            
+        if  mapa[x][y].isdigit() and mapa[x][y + 1].isdigit() and mapa[x][y + 2].isdigit() and mapa[x][y + 3].isdigit() and current == 'D':
+            numero_completo = mapa[x][y] + mapa[x][y +1] + mapa[x][y + 2] + mapa[x][y + 3]
+            print(int(numero_completo), end=(', '))
+            count += int(numero_completo)
+            mapa[x][y] = 'x'
+            mapa[x][y + 1] = 'x'
+            mapa[x][y + 2] = 'x'
+            mapa[x][y + 3] = 'x'
+
+        if  mapa[x][y].isdigit() and mapa[x][y - 1].isdigit() and mapa[x][y - 2].isdigit() and current == 'E':
+            numero_completo = mapa[x][y - 2] + mapa[x][y - 1] + mapa[x][y]
+            print(int(numero_completo), end=(', '))
+            count += int(numero_completo)
+            mapa[x][y] = 'x'
+            mapa[x][y - 1] = 'x'
+            mapa[x][y - 2] = 'x'
+
+        if  mapa[x][y].isdigit() and mapa[x][y + 1].isdigit() and mapa[x][y + 2].isdigit() and current == 'D':
+            numero_completo = mapa[x][y] + mapa[x][y +1] + mapa[x][y + 2]
+            print(int(numero_completo), end=(', '))
+            count += int(numero_completo)
+            mapa[x][y] = 'x'
+            mapa[x][y + 1] = 'x'
+            mapa[x][y + 2] = 'x'
+
+        if  mapa[x][y].isdigit() and mapa[x][y - 1].isdigit() and current == 'E':
+            numero_completo = mapa[x][y - 1] + mapa[x][y]
+            print(int(numero_completo), end=(', '))
+            count += int(numero_completo)
+            mapa[x][y] = 'x'
+            mapa[x][y - 1] = 'x'
+
+        if  mapa[x][y].isdigit() and mapa[x][y + 1].isdigit() and current == 'D':
+            numero_completo = mapa[x][y] + mapa[x][y +1]
+            print(int(numero_completo), end=(', '))
+            count += int(numero_completo)
+            mapa[x][y] = 'x'
+            mapa[x][y + 1] = 'x'
+
         if mapa[x][y].isdigit():
             print(mapa[x][y], end=(', '))
             count += int(mapa[x][y])
             mapa[x][y] = 'x'
-
+       
         if current == 'D':
             y += 1
         elif current == 'E':
@@ -27,7 +78,7 @@ def contar_dinheiro(mapa):
             x -= 1
         elif current == 'B':
             x += 1
-        
+       
         if mapa[x][y] == '/' and current == 'D':
             x -= 1
             current = 'C'
@@ -35,10 +86,10 @@ def contar_dinheiro(mapa):
             x -= 1
             current = 'C'
         elif mapa[x][y] == '/' and current == 'E':
-            x += 1 
+            x += 1
             current = 'B'
         elif mapa[x][y] == '\\' and current == 'D':
-            x += 1 
+            x += 1
             current = 'B'
         elif mapa[x][y] == '\\' and current == 'C':
             y -= 1
@@ -53,12 +104,14 @@ def contar_dinheiro(mapa):
             y += 1
             current = 'D'
 
+
     return count
+
 
 def main():
     with open('casoG50.txt', 'r') as arquivo:
         conteudo = arquivo.readlines()
-        
+       
     for linha in conteudo:
         linhamapa = []
         for i in linha:
@@ -66,9 +119,12 @@ def main():
                 linhamapa.append(i)
         mapa.append(linhamapa)
 
+
     print('Ordem em que o dinheiro foi encontrado: ')
     print('\nA quantida total de dinheiro recuperada é: ', contar_dinheiro(mapa))
 
+
 if __name__ == "__main__":
     main()
+
 
